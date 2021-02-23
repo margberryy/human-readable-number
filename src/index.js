8475627b1676a08b91bae6e6f5e7ef32d2e8ddbc
Marg <paerl.mag@gmail.com>
2021-02-23 13:42:04 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
+  let numString = String(number);
+  let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
+    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let hundred = ' hundred';
+  let numReadable = '';
+  if (numString.charAt(0) === "0") {
+    return "zero";
+  } 
+  else if (numString.length < 3 && number < 20) {
+    return ones[number];
+  } 
   
+  else if  (number < 100) {
+    numReadable = tens[numString.charAt(0)]+ " " + ones[numString.charAt(1)];
+  } 
+  
+  else if (number < 1000 && numString.charAt(1) < 2 && numString.charAt(1) > 0) {
+    numReadable = ones[numString.charAt(0)] + hundred + " "+ ones[numString.charAt(1) + numString.charAt(2)];
+  } 
+  
+  else if (numString.charAt(1) == 0){
+    numReadable = ones[numString.charAt(0)]+ hundred +  " "+ ones[numString.charAt(2)];
+  
+  } else{
+    numReadable = ones[numString.charAt(0)]+ hundred + " " + tens[numString.charAt(1)] + " "+ ones[numString.charAt(2)];
+  }
+  return numReadable.trim();
 }
